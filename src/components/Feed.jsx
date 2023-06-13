4a3680aba15c295875a57133ded5c09c30ea720e
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-13 14:11:52 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,6 +13,12 @@ const Feed = () => {
           Copyright 2023 Anirudh Salaria
         </Typography>
       </Box>
+
+      <Box p={2} sx={{overflowY: 'auto',height:'90vh',flex:2}}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color:'white'}}>
+          New <span style={{color:'#F31503'}}>videos</span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
