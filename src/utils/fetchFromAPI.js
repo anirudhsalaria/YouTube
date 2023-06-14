34bcfa5fd1f32c81a50ce00fd272bf8c54203489
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-14 21:45:54 +0530

    update

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    url: BASE_URL,
     params: {
       maxResults: '50'
     },
