7d74d306c02e12b063dd44fffb2e7ba983b43ebc
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-14 14:22:01 +0530

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -19,6 +19,10 @@ const ChannelCard = (channelDetail) => {
           alt={channelDetail?.snippet?.title}
           sx={{borderRadius:'50%',height:'180px',width:'180px',mb:2,border:'1px solid #e3e3e3'}}
           />
+          <Typography variant='h6'>
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{fontSize:14,color:'gray',ml:'5px'}}/>
+          </Typography>
         </CardContent>
       </Link>
     </Box>
