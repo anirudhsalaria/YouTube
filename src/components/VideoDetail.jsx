51a93d42cb2f75b66c66d8ef23e6c178bd767915
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-16 15:02:53 +0530

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -15,9 +15,11 @@ const VideoDetail = () => {
       .then((data) => {setVideoDetail(data.items[0])})
   },[id]);
 
+  if(!videoDetail?.snippet) return 'Loading...';
+
   console.log(videoDetail);
 
-  const {snippet} = videoDetail
+  const {snippet:{title,channelId,channelTitle},statistics:{viewCount,likeCount}} = videoDetail;
 
   return (
     <Box minHeight="95vh">
@@ -26,8 +28,25 @@ const VideoDetail = () => {
           <Box sx={{width:'100%',position:'sticky',top:'86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
             <Typography color='#fff' variant="h5" fontWeight="bold" p={2}>
-              {snippet?.title}
+              {title}
             </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{color:"#fff"}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{sm: 'subtitle1',md: 'h6'}} color='#fff'>
+                  {channelTitle}
+                  <CheckCircle sx={{fontSize: '12px',color:'gray',ml:'5px'}}/>
+                </Typography>
+              </Link>
+              <Stack direction="row" gap='20px' alignItems='center'>
+                <Typography variant="body1" sx={{opacity:'0.7'}}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+
+                <Typography variant="body1" sx={{opacity:'0.7'}}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
         </Box>
       </Stack>
